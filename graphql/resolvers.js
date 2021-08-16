ab58b01b7e558d3db1e1d5b4824c43c9e6fc9486
Babou360 <leebabou@gmail.com>
2021-08-16 18:00:58 +0300

    registration

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,4 +1,6 @@
 const {User} = require('../models/index')
+const bcrypt = require('bcryptjs')
+const {UserInputError} = require('apollo-server')
 
 module.exports = {
     Query: {
@@ -12,4 +14,38 @@ module.exports = {
         }
       }
     },
+
+    Mutation: {
+      register: async(_,args) => {
+        let {username, email, password} = args
+        let errors ={}
+
+        try{
+          if(email.trim() === '') errors.email = 'email cannot be empty'
+          if(username.trim() === '') errors.username = 'username cannot be empty'
+          if(password.trim() === '') errors.password = 'password cannot be empty'
+
+          const userbyName = await User.findOne({where: {username}})
+          const userbyEmail = await User.findOne({where: {email}})
+
+          if(userbyName) errors.username = 'username is taken'
+          if(userbyEmail) errors.email = 'email is taken'
+
+          if(Object.keys(errors).length > 0){
+            throw errors
+          }
+
+          password = await bcrypt.hash(password,6)
+          const user = await User.create({
+            username,
+            email,
+            password
+          })
+          return user;
+
+        }catch(err){
+          throw new  UserInputError('Bad input',{errors: err})
+        }
+      }
+    }
   };
--- graphql/schema.js
+++ graphql/schema.js
@@ -5,8 +5,13 @@ module.exports = gql`
   type User{
     username: String!
     email: String!
+    password: String!
   }
   type Query {
     getUsers: [User]!
+  },
+  
+  type Mutation{
+    register(username: String! email: String! password: String!): User! 
   }
 `;
